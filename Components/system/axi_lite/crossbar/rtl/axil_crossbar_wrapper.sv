b5d278adc4e7d8d61aa1aadd3c05534585072922
filssavi <filippo.savi@nottingham.edu.cn>
2021-12-20 17:06:29 +0100

    Added license to axi lite crossbar wrapper

--- Components/system/axi_lite/crossbar/rtl/axil_crossbar_wrapper.sv
+++ Components/system/axi_lite/crossbar/rtl/axil_crossbar_wrapper.sv
@@ -1,31 +1,42 @@
+// Copyright 2021 Filippo Savi
+// Author: Filippo Savi <filssavi@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-
-
-module	axil_crossbar_interface #(
-		parameter integer DATA_WIDTH = 32,
-		parameter integer ADDR_WIDTH = 32,
-		parameter	NM = 4,
-		parameter	NS = 8,
-		parameter	[NS*ADDR_WIDTH-1:0]	SLAVE_ADDR = {
-			3'b111,  {(ADDR_WIDTH-3){1'b0}},
-			3'b110,  {(ADDR_WIDTH-3){1'b0}},
-			3'b101,  {(ADDR_WIDTH-3){1'b0}},
-			3'b100,  {(ADDR_WIDTH-3){1'b0}},
-			3'b011,  {(ADDR_WIDTH-3){1'b0}},
-			3'b010,  {(ADDR_WIDTH-3){1'b0}},
-			4'b0001, {(ADDR_WIDTH-4){1'b0}},
-			4'b0000, {(ADDR_WIDTH-4){1'b0}} },
-		parameter	[NS*ADDR_WIDTH-1:0]	SLAVE_MASK = (NS <= 1) ? { 4'b1111, {(ADDR_WIDTH-4){1'b0}} } : { {(NS-2){ 3'b111, {(ADDR_WIDTH-3){1'b0}} }}, {(2){ 4'b1111, {(ADDR_WIDTH-4){1'b0}} }} },
-		parameter [0:0]	OPT_LOWPOWER = 1,
-		parameter	OPT_LINGER = 4,
-		parameter	LGMAXBURST = 5
-	) (
-
-		input wire clock,
-		input wire reset,
+module axil_crossbar_interface #(
+        parameter integer DATA_WIDTH = 32,
+        parameter integer ADDR_WIDTH = 32,
+        parameter NM = 4,
+        parameter NS = 8,
+        parameter [NS*ADDR_WIDTH-1:0] SLAVE_ADDR = {
+            3'b111,  {(ADDR_WIDTH-3){1'b0}},
+            3'b110,  {(ADDR_WIDTH-3){1'b0}},
+            3'b101,  {(ADDR_WIDTH-3){1'b0}},
+            3'b100,  {(ADDR_WIDTH-3){1'b0}},
+            3'b011,  {(ADDR_WIDTH-3){1'b0}},
+            3'b010,  {(ADDR_WIDTH-3){1'b0}},
+            4'b0001, {(ADDR_WIDTH-4){1'b0}},
+            4'b0000, {(ADDR_WIDTH-4){1'b0}} },
+        parameter [NS*ADDR_WIDTH-1:0] SLAVE_MASK = (NS <= 1) ? { 4'b1111, {(ADDR_WIDTH-4){1'b0}} } : { {(NS-2){ 3'b111, {(ADDR_WIDTH-3){1'b0}} }}, {(2){ 4'b1111, {(ADDR_WIDTH-4){1'b0}} }} },
+        parameter [0:0] OPT_LOWPOWER = 1,
+        parameter OPT_LINGER = 4,
+        parameter LGMAXBURST = 5
+    ) (
+        input wire clock,
+        input wire reset,
         axi_lite.slave slaves [NM-1:0],
         axi_lite.master masters [NS-1:0]
-	);
+    );
 
     localparam STROBE_WIDTH = DATA_WIDTH/8;
 
@@ -109,7 +120,7 @@ module	axil_crossbar_interface #(
     wire [NS*3-1:0] M_AXI_ARPROT;
 
     generate
-        for(i = 0; i< NM; i = i + 1) begin
+        for(i = 0; i< NS; i = i + 1) begin
             
             // WRITE ADDRESS CHANNEL FLATTENING
             assign masters[i].AWVALID = M_AXI_AWVALID[i];
