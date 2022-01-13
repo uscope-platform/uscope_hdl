82444efb43db9c487bb770c1e947143c3b435f42
Filippo Savi <filisavi@unimore.it>
2022-01-13 13:02:00 +0100

    Split input and output ff stages parameter in handshake axi stream CDC

--- Components/system/axi_stream/cdc/rtl/axis_cdc.sv
+++ Components/system/axi_stream/cdc/rtl/axis_cdc.sv
@@ -16,7 +16,14 @@
 `timescale 10ns / 1ns
 `include "interfaces.svh"
 
-module axis_cdc #(parameter CDC_STYLE = "FF", N_STAGES = 3, DATA_WIDTH = 32, USER_WIDTH = 32, DEST_WIDTH = 32)(
+module axis_cdc #(
+parameter CDC_STYLE = "FF",
+    N_STAGES_IN = 2,
+    N_STAGES_OUT = 3,
+    DATA_WIDTH = 32,
+    USER_WIDTH = 32, 
+    DEST_WIDTH = 32
+  )(
     input wire reset,
     input wire clock_in,
     input wire clock_out,
@@ -24,17 +31,24 @@ module axis_cdc #(parameter CDC_STYLE = "FF", N_STAGES = 3, DATA_WIDTH = 32, USE
     axi_stream.master out
     );
 
-    reg [DATA_WIDTH-1:0] data_meta[N_STAGES-1:0];
-    reg [USER_WIDTH-1:0] user_meta[N_STAGES-1:0];
-    reg [DEST_WIDTH-1:0] dest_meta[N_STAGES-1:0];
-    reg [N_STAGES-1:0] valid_meta, ready_meta;
 
-    assign out.data = data_meta[N_STAGES-1];
-    assign out.user = user_meta[N_STAGES-1];
-    assign out.dest = dest_meta[N_STAGES-1];
-    assign out.valid = valid_meta[N_STAGES-1];
+
+
     generate
     if(CDC_STYLE == "FF")begin
+    
+        localparam N_STAGES = N_STAGES_IN;
+        reg [DATA_WIDTH-1:0] data_meta[N_STAGES-1:0];
+        reg [USER_WIDTH-1:0] user_meta[N_STAGES-1:0];
+        reg [DEST_WIDTH-1:0] dest_meta[N_STAGES-1:0];
+        reg [N_STAGES-1:0] valid_meta, ready_meta;
+        
+        assign out.data = data_meta[N_STAGES-1];
+        assign out.user = user_meta[N_STAGES-1];
+        assign out.dest = dest_meta[N_STAGES-1];
+        assign out.valid = valid_meta[N_STAGES-1];
+        
+
         always @(posedge clock_out) begin
             for(integer i = 0; i< N_STAGES; i = i+1)begin
                 data_meta[i+1] <= data_meta[i];
@@ -60,10 +74,11 @@ module axis_cdc #(parameter CDC_STYLE = "FF", N_STAGES = 3, DATA_WIDTH = 32, USE
         end
 
     end else if(CDC_STYLE == "HANDSHAKE")begin
-        
+
         reg cdc_ready_b, cdc_valid_b;
         always@(posedge clock_in)begin
-            if(~reset)begin{ in.ready, ready_meta } <= { ready_meta, out.ready};
+            if(~reset)begin
+
                 cdc_valid_b <= 0;
                 in.ready <= 1;
             end else begin
@@ -80,14 +95,34 @@ module axis_cdc #(parameter CDC_STYLE = "FF", N_STAGES = 3, DATA_WIDTH = 32, USE
             end
         end
 
+    // xpm_cdc_array_single #(
+    //     .DEST_SYNC_FF(4),   // DECIMAL; range: 2-10
+    //     .INIT_SYNC_FF(0),   // DECIMAL; 0=disable simulation init values, 1=enable simulation init values
+    //     .SIM_ASSERT_CHK(0), // DECIMAL; 0=disable simulation messages, 1=enable simulation messages
+    //     .SRC_INPUT_REG(1),  // DECIMAL; 0=do not register input, 1=register input
+    //     .WIDTH(2)           // DECIMAL; range: 1-1024
+    // )
+    // xpm_cdc_array_single_inst (
+    //     .dest_out(dest_out), // WIDTH-bit output: src_in synchronized to the destination clock domain. This
+    //                         // output is registered.
+
+    //     .dest_clk(dest_clk), // 1-bit input: Clock signal for the destination clock domain.
+    //     .src_clk(src_clk),   // 1-bit input: optional; required when SRC_INPUT_REG = 1
+    //     .src_in(src_in)      // WIDTH-bit input: Input single-bit array to be synchronized to destination clock
+    //                         // domain. It is assumed that each bit of the array is unrelated to the others. This
+    //                         // is reflected in the constraints applied to this macro. To transfer a binary value
+    //                         // losslessly across the two clock domains, use the XPM_CDC_GRAY macro instead.
+
+    // );
+
 
 
         xpm_cdc_handshake #(
             .DEST_EXT_HSK(0),
-            .DEST_SYNC_FF(2),
+            .DEST_SYNC_FF(N_STAGES_OUT),
             .INIT_SYNC_FF(1),
             .SIM_ASSERT_CHK(1),
-            .SRC_SYNC_FF(2),
+            .SRC_SYNC_FF(N_STAGES_IN),
             .WIDTH(DATA_WIDTH)
         ) op_cdc_data (
             .dest_out(out.data),
@@ -102,10 +137,10 @@ module axis_cdc #(parameter CDC_STYLE = "FF", N_STAGES = 3, DATA_WIDTH = 32, USE
 
         xpm_cdc_handshake #(
             .DEST_EXT_HSK(0),
-            .DEST_SYNC_FF(2),
+            .DEST_SYNC_FF(N_STAGES_OUT),
             .INIT_SYNC_FF(1),
             .SIM_ASSERT_CHK(1),
-            .SRC_SYNC_FF(2),
+            .SRC_SYNC_FF(N_STAGES_IN),
             .WIDTH(DATA_WIDTH)
         ) op_cdc_dest (
             .dest_out(out.dest),
