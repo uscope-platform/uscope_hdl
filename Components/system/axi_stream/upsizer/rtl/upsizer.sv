ac5fde54001b7f80eebcb7699a55cdc5100c22a2
filippo savi <filippo.savi@unimore.it>
2024-02-26 14:58:30 +0100

    bypass upsizer when input and output data width match

--- Components/system/axi_stream/upsizer/rtl/upsizer.sv
+++ Components/system/axi_stream/upsizer/rtl/upsizer.sv
@@ -28,62 +28,76 @@ module upsizer#(
     axi_stream.master data_out
 );
 
-    axi_stream #(.DATA_WIDTH(INPUT_WIDTH), .DEST_WIDTH(DEST_WIDTH), .USER_WIDTH(USER_WIDTH)) data_in_buffered();
+
+    generate
+        if(INPUT_WIDTH == OUTPUT_WIDTH)begin
+
+            assign data_in.ready = data_out.ready;
+            assign data_out.dest = data_in.dest;
+            assign data_out.tlast = data_in.tlast;
+            assign data_out.user = data_in.user;
+            assign data_out.data = data_in.data;
+
+        end else begin
+
+            axi_stream #(.DATA_WIDTH(INPUT_WIDTH), .DEST_WIDTH(DEST_WIDTH), .USER_WIDTH(USER_WIDTH)) data_in_buffered();
 
 
-    axis_skid_buffer #(
-        .REGISTER_OUTPUT(0),
-        .LATCHING(0),
-        .DATA_WIDTH(INPUT_WIDTH),
-        .DEST_WIDTH(DEST_WIDTH),
-        .USER_WIDTH(USER_WIDTH)
-    ) backpressure_buffer (
-        .clock(clock),
-        .reset(reset),
-        .axis_in(data_in),
-        .axis_out(data_in_buffered)
-    );
+            axis_skid_buffer #(
+                .REGISTER_OUTPUT(0),
+                .LATCHING(0),
+                .DATA_WIDTH(INPUT_WIDTH),
+                .DEST_WIDTH(DEST_WIDTH),
+                .USER_WIDTH(USER_WIDTH)
+            ) backpressure_buffer (
+                .clock(clock),
+                .reset(reset),
+                .axis_in(data_in),
+                .axis_out(data_in_buffered)
+            );
 
-    localparam UPSIZING_RATIO = OUTPUT_WIDTH/INPUT_WIDTH;
+            localparam UPSIZING_RATIO = OUTPUT_WIDTH/INPUT_WIDTH;
 
-    reg [INPUT_WIDTH-1:0] input_data_buffer [UPSIZING_RATIO-2:0];
-    reg [DEST_WIDTH-1:0] input_dest_buffer [UPSIZING_RATIO-2:0];
-    reg [USER_WIDTH-1:0] input_user_buffer [UPSIZING_RATIO-2:0];
+            reg [INPUT_WIDTH-1:0] input_data_buffer [UPSIZING_RATIO-2:0];
+            reg [DEST_WIDTH-1:0] input_dest_buffer [UPSIZING_RATIO-2:0];
+            reg [USER_WIDTH-1:0] input_user_buffer [UPSIZING_RATIO-2:0];
 
-    reg [$clog2(UPSIZING_RATIO)-1:0] fill_counter = 0;
+            reg [$clog2(UPSIZING_RATIO)-1:0] fill_counter = 0;
 
-    assign data_in_buffered.ready = data_out.ready;
+            assign data_in_buffered.ready = data_out.ready;
 
-    enum reg { 
-        fill_buffer = 0,
-        push_output = 1
-    } upsizer_fsm = fill_buffer;
+            enum reg { 
+                fill_buffer = 0,
+                push_output = 1
+            } upsizer_fsm = fill_buffer;
 
-    always_ff @(posedge clock)begin
-        case (upsizer_fsm)
-            fill_buffer:begin
-                data_out.valid <= 0 ;
-                if(data_in_buffered.valid & data_out.ready)begin
-                    if(fill_counter == UPSIZING_RATIO-2)begin
-                        upsizer_fsm <= push_output;
-                    end else begin
-                        fill_counter <= fill_counter + 1;
-                    end
-                    input_data_buffer[fill_counter] <= data_in_buffered.data;
-                    input_dest_buffer[fill_counter] <= data_in_buffered.dest;
-                    input_user_buffer[fill_counter] <= data_in_buffered.user;
-                end
-            end 
-            push_output:begin
-                if(data_in_buffered.valid & data_out.ready)begin
-                    upsizer_fsm <= fill_buffer;
-                    data_out.valid <= 1;
-                    data_out.data <= {data_in_buffered.data, input_data_buffer[0]};
-                    data_out.dest <= input_dest_buffer[0];
-                    data_out.user <= input_user_buffer[0];
-                end
-            end 
-        endcase
-    end
+            always_ff @(posedge clock)begin
+                case (upsizer_fsm)
+                    fill_buffer:begin
+                        data_out.valid <= 0 ;
+                        if(data_in_buffered.valid & data_out.ready)begin
+                            if(fill_counter == UPSIZING_RATIO-2)begin
+                                upsizer_fsm <= push_output;
+                            end else begin
+                                fill_counter <= fill_counter + 1;
+                            end
+                            input_data_buffer[fill_counter] <= data_in_buffered.data;
+                            input_dest_buffer[fill_counter] <= data_in_buffered.dest;
+                            input_user_buffer[fill_counter] <= data_in_buffered.user;
+                        end
+                    end 
+                    push_output:begin
+                        if(data_in_buffered.valid & data_out.ready)begin
+                            upsizer_fsm <= fill_buffer;
+                            data_out.valid <= 1;
+                            data_out.data <= {data_in_buffered.data, input_data_buffer[0]};
+                            data_out.dest <= input_dest_buffer[0];
+                            data_out.user <= input_user_buffer[0];
+                        end
+                    end 
+                endcase
+            end
+        end
+    endgenerate
 
 endmodule
