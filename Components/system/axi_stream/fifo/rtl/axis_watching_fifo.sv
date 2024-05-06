ffe9977633c61043bcc357ae0575f4f6535b44df
filippo savi <filippo.savi@unimore.it>
2024-05-06 17:06:04 +0200

    add watching combiner and fifos

---
+++ Components/system/axi_stream/fifo/rtl/axis_watching_fifo.sv
@@ -0,0 +1,84 @@
+// Copyright 2021 University of Nottingham Ningbo China
+// Author: Filippo Savi <filssavi@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+`timescale 10ns / 1ns
+`include "interfaces.svh"
+
+module axis_watching_fifo #(parameter DATA_WIDTH = 32,USER_WIDTH = 32, DEST_WIDTH = 32, FIFO_DEPTH = 16)(
+    input wire clock,
+    input wire reset,
+    axi_stream.watcher in,
+    axi_stream.master out
+);
+
+
+reg [$clog2(FIFO_DEPTH)-1:0] hp_mem_addr_w;
+reg [$clog2(FIFO_DEPTH)-1:0] hp_mem_addr_r;
+
+reg [$clog2(FIFO_DEPTH):0] fill_level;
+
+reg [DATA_WIDTH-1:0] fifo_memory[FIFO_DEPTH-1:0];
+reg [DEST_WIDTH-1:0] dest_memory[FIFO_DEPTH-1:0];
+reg [USER_WIDTH-1:0] user_memory[FIFO_DEPTH-1:0];
+integer i;
+
+always@(posedge clock) begin
+    if(~reset)begin
+        for(i = 0; i< FIFO_DEPTH; i = i+1)begin
+            fifo_memory[i] = 0;
+        end
+        for(i = 0; i< FIFO_DEPTH; i = i+1)begin
+            dest_memory[i] = 0;
+        end 
+        for(i = 0; i< FIFO_DEPTH; i = i+1)begin
+            user_memory[i] = 0;
+        end 
+        fill_level <=0;
+        hp_mem_addr_w <=0;
+        hp_mem_addr_r <=0;
+        out.data <= 0;
+        out.valid <= 0;
+        out.tlast <= 0;
+    end else begin
+        out.valid <= 0;
+        out.tlast <= 0;
+        if(out.ready)begin
+            if(fill_level != 0)begin
+                {out.tlast,out.data[DATA_WIDTH-1:0]} <= fifo_memory[hp_mem_addr_r];
+                out.dest <= dest_memory[hp_mem_addr_r];
+                out.user <= user_memory[hp_mem_addr_r];
+                out.valid <=1;
+                if(out.valid)begin
+                    if(hp_mem_addr_r == FIFO_DEPTH-1) hp_mem_addr_r <= 0;
+                    else hp_mem_addr_r <= hp_mem_addr_r+1;
+                    fill_level<= fill_level-1;
+                end
+            end
+        end
+        if(in.valid)begin
+            fifo_memory[hp_mem_addr_w] <= {in.tlast, in.data};
+            dest_memory[hp_mem_addr_w] <= in.dest;
+            user_memory[hp_mem_addr_w] <= in.user;
+            fill_level<= fill_level+1;
+            if(hp_mem_addr_w == FIFO_DEPTH-1) hp_mem_addr_w <= 0;
+            else hp_mem_addr_w <= hp_mem_addr_w+1;
+            
+        end
+
+    end
+end
+
+
+endmodule
---
+++ Components/system/axi_stream/fifo/rtl/axis_watching_fifo_xpm.sv
@@ -0,0 +1,59 @@
+// Copyright 2021 University of Nottingham Ningbo China
+// Author: Filippo Savi <filssavi@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+`timescale 10ns / 1ns
+`include "interfaces.svh"
+
+module axis_watching_fifo_xpm #(
+   parameter DATA_WIDTH = 32,
+   DEST_WIDTH=16,
+   USER_WIDTH = 32,
+   FIFO_DEPTH = 16,
+   SIM_ASSERT_CHK=1
+)(
+   input wire clock,
+   input wire reset,
+   axi_stream.watcher in,
+   axi_stream.master out
+);
+
+
+   xpm_fifo_axis #(
+      .FIFO_DEPTH(FIFO_DEPTH),
+      .PACKET_FIFO("false"),
+      .SIM_ASSERT_CHK(SIM_ASSERT_CHK),
+      .TDATA_WIDTH(DATA_WIDTH),
+      .TDEST_WIDTH(DEST_WIDTH),
+      .TUSER_WIDTH(USER_WIDTH),
+      .USE_ADV_FEATURES("0000")
+   )
+   xpm_fifo_axis_inst (
+      .m_axis_tdata(out.data),
+      .m_axis_tdest(out.dest),
+      .m_axis_tlast(out.tlast),
+      .m_axis_tuser(out.user),
+      .m_axis_tvalid(out.valid),
+      .m_aclk(clock),
+      .m_axis_tready(out.ready),
+      .s_aclk(clock),
+      .s_aresetn(reset),
+      .s_axis_tdata(in.data), 
+      .s_axis_tdest(in.dest),
+      .s_axis_tuser(in.user),
+      .s_axis_tlast(in.tlast),
+      .s_axis_tvalid(in.valid)
+   );
+
+endmodule
