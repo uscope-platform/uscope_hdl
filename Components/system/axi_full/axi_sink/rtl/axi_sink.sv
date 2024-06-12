a9de4ceb18d472048bdb5c64b749214fc22366f3
Filippo Savi <filisavi@unimore.it>
2024-06-12 11:43:34 +0200

    Reimplemented axi full sink allowing configurable latency between transfer stages

--- Components/system/axi_full/axi_sink/rtl/axi_sink.sv
+++ Components/system/axi_full/axi_sink/rtl/axi_sink.sv
@@ -21,7 +21,8 @@ module axi_full_slave_sink #(
     parameter BUFFER_SIZE = 6144,
     parameter MEMORY_WIDTH = 64,
     parameter BASE_ADDRESS = 0,
-    parameter BVALID_LATENCY = 3
+    parameter BVALID_LATENCY = 3,
+    parameter ADDR_TO_WREADY_LATENCY = 2
 )(
     input wire        clock,
     input wire        reset,
@@ -42,54 +43,72 @@ module axi_full_slave_sink #(
 
     reg [MEMORY_WIDTH-1:0] memory_map [BUFFER_SIZE-1:0];
 
+
+
+    enum reg [2:0] {
+        wait_addr = 0,       
+        addr_to_data_latency = 1,
+        data_transfer = 2,
+        response_latency = 3
+    } transfer_stage = wait_addr;
+
+
+    reg [15:0] addr_to_ready_ctr = 0;
+    reg [15:0] burst_ctr = 0;
+    reg [7:0] burst_len = 0;
+    reg [15:0] bvalid_ctr = 0;
+
+
+    initial begin
+        axi_in.WREADY <= 1'b0;
+        axi_in.AWREADY <= 1'b1;
+        axi_in.ARREADY <= 1'b1;
+        axi_in.BRESP <= 0;
+    end
+
     always @( posedge clock ) begin : wready_generation
-        if ( reset == 1'b0 ) begin
-            axi_in.WREADY <= 1'b1;
-            axi_in.AWREADY <= 1'b1;
-            axi_in.ARREADY <= 1'b1;
-        end else begin    
-        end 
-    end       
-
-    reg bvalid_wait = 0;
-    reg [7:0] bvalid_counter = 0;
-
-
-    always @( posedge clock ) begin : write_response_generation
-        if ( reset == 1'b0 ) begin
-            axi_in.BVALID <= 0;
-            axi_in.BID <= 0;
-            axi_in.BUSER <= 0;
-            axi_in.BRESP <= 2'b0;
-        end else begin
-            if(axi_in.AWVALID)begin
-                current_base_address <= axi_in.AWADDR;
-                burst_address <= 0;
-                burst_counter++;
+        case(transfer_stage)
+            wait_addr:begin
+                axi_in.BVALID <= 0;
+                if(axi_in.AWVALID) begin
+                    transfer_stage <= addr_to_data_latency;
+                    burst_len <= axi_in.AWLEN;
+                end
             end
-            if(axi_in.WVALID)begin
-                burst_address <= burst_address +1;
-                memory_map[memory_index] <= axi_in.WDATA;
+            addr_to_data_latency:begin
+                if(addr_to_ready_ctr == ADDR_TO_WREADY_LATENCY-1)begin
+                    transfer_stage <= data_transfer;
+                    addr_to_ready_ctr <= 0;
+                    axi_in.WREADY <= 1'b1;
+                end else begin
+                    addr_to_ready_ctr <= addr_to_ready_ctr + 1;
+                end
             end
-            axi_in.BVALID <= 0;
-            case (bvalid_wait)
-                0:begin
-                    if(axi_in.WVALID && axi_in.WREADY)begin
-                        bvalid_wait <=1;
-                        bvalid_counter <= 0;
+            data_transfer:begin
+                if(burst_ctr == burst_len)begin
+                    if(axi_in.WLAST)begin
+                        transfer_stage <= response_latency;
+                        axi_in.WREADY <= 1'b0;
+                        burst_ctr <= 0;
+                    end
+                end else begin
+                    if(axi_in.WVALID & axi_in.WREADY) begin
+                        burst_ctr <= burst_ctr + 1;
                     end
                 end
-                1:begin
-                    if(bvalid_counter == BVALID_LATENCY-1)begin
-                        axi_in.BVALID <= 1;
-                        bvalid_wait <= 0;
-                    end else begin
-                        bvalid_counter <= bvalid_counter+1;
-                    end
-                end 
-            endcase
-        end 
-    end   
+            end
+            response_latency:begin
+                if(bvalid_ctr == BVALID_LATENCY)begin
+                    axi_in.BVALID <= 1;
+                    bvalid_ctr <= 0;
+                    transfer_stage <= wait_addr;
+                end else begin
+                    bvalid_ctr <= bvalid_ctr + 1;
+                end
+            end
+        endcase
+    end       
+
   
     integer transfers_counter = 0;
 
