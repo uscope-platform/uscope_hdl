c0db50594a74331072440057e35e572189e0a370
Filippo Savi <filssavi@gmail.com>
2026-03-25 13:12:41 +0100

    start implmenting a formal faiult handling unit

---
+++ Components/system/fault_managemnt_unit/rtl/fault_managemnt_unit.sv
@@ -0,0 +1,28 @@
+// Copyright 2026 Filippo Savi
+// Author: Filippo Savi <filssavi@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+`timescale 10ns / 1ns
+
+module fault_managemnt_unit #(
+    int N_FAULTS = 6
+)(
+    input wire clock,
+    input wire reset,
+    input wire [N_FAULTS-1:0] fault_in,
+    output wire fault_out,
+    output reg clear_fault,
+    axi_lite.slave axi_in
+);
+
+endmodule
